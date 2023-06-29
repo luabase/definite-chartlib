70c406167d28e9fb7458a1050e4ee1a9a2655342
Michael Harris <mharris@luabase.com>
2023-06-29 16:40:03 +0000

    feat: add more types

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,8 +1,49 @@
 export enum ChartType {
   BAR = "bar",
+  CALENDAR = "calendar",
+  HEATMAP = "heatmap",
   LINE = "line",
   PIE = "pie",
   SCATTER = "scatter",
-  HEATMAP = "heatmap",
-  CALENDAR = "calendar",
+}
+
+export enum DataType {
+  CATEGORY = "category",
+  DATETIME = "datetime",
+  VALUE = "value",
+}
+
+export enum AggregationType {
+  AVG = "avg",
+  COUNT = "count",
+  DISTINCT = "distinct",
+  SUM = "sum",
+  MIN = "min",
+  MAX = "max",
+  NONE = "none",
+}
+
+export enum AxisType {
+  LEFT = "left",
+  RIGHT = "right",
+}
+
+export enum OrientationType {
+  VERTICAL = "vertical",
+  HORIZONTAL = "horizontal",
+}
+
+export enum BarStyleType {
+  GROUPED = "grouped",
+  STACKED = "stacked",
+}
+
+export enum LineStyleType {
+  POINT = "point",
+  SMOOTH = "smooth",
+}
+
+export enum ColorGroupingType {
+  CONTINUOUS = "continuous",
+  PIECEWISE = "piecewise",
 }
