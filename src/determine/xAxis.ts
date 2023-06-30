ba8cf53ee54832d942d37f5067160cc0bca26435
Michael Harris <mharris@luabase.com>
2023-06-30 21:37:23 +0000

    chore: push to const axes

--- src/determine/xAxis.ts
+++ src/determine/xAxis.ts
@@ -7,34 +7,34 @@ export function xAxis<T extends ChartType>(
   opts: ChartConfigOptions<T>,
   df: DataFrame
 ): echarts.Axis[] {
+  const axes: echarts.Axis[] = [];
   switch (opts.chartType) {
     case "bar":
       switch ((<StyleOptions<"bar">>{ ...opts.style }).orientation) {
         case "vertical": {
           const dim = opts.dimensions[0];
-          return [
-            {
-              type: "category",
-              show: true,
-              name: df.columns.get(dim.index) ?? "",
-              nameLocation: "center",
-              nameGap: df.shape.height > 6 ? 55 : 30,
-              nameTextStyle: {
-                fontSize: 14,
-              },
-              axisLabel: {
-                interval: Math.floor((df.shape.height - 1) / 10),
-                rotate: df.shape.height > 6 ? 30 : 0,
-                formatter: categoryFormatter,
-              },
+          axes.push({
+            type: "category",
+            show: true,
+            name: df.columns.get(dim.index) ?? "",
+            nameLocation: "center",
+            nameGap: df.shape.height > 6 ? 55 : 30,
+            nameTextStyle: {
+              fontSize: 14,
             },
-          ];
+            axisLabel: {
+              interval: Math.floor((df.shape.height - 1) / 10),
+              rotate: df.shape.height > 6 ? 30 : 0,
+              formatter: categoryFormatter,
+            },
+          });
+          break;
         }
-        case "horizontal": {
-          // FIXME: handle multiple axes
-          const metric = opts.metrics[0];
-          return [
-            {
+        case "horizontal":
+          {
+            // FIXME: handle multiple axes
+            const metric = opts.metrics[0];
+            axes.push({
               type: "value",
               show: true,
               name: df.columns.get(metric.index) ?? "",
@@ -46,51 +46,50 @@ export function xAxis<T extends ChartType>(
               axisLabel: {
                 formatter: valueFormatter,
               },
-            },
-          ];
-        }
+            });
+          }
+          break;
       }
       break;
     case "scatter": {
       const metric = opts.metrics[0];
-      return [
-        {
-          type: "value",
-          show: true,
-          name: df.columns.get(metric.index) ?? "",
-          nameLocation: "center",
-          nameGap: 30,
-          nameTextStyle: {
-            fontSize: 14,
-          },
-          splitLine: {
-            lineStyle: {
-              color: color.ZINC_800,
-              type: "dashed",
-              width: 1,
-            },
-            show: true,
-          },
-          axisLabel: {
-            formatter: valueFormatter,
-          },
+      axes.push({
+        type: "value",
+        show: true,
+        name: df.columns.get(metric.index) ?? "",
+        nameLocation: "center",
+        nameGap: 30,
+        nameTextStyle: {
+          fontSize: 14,
         },
-      ];
+        splitLine: {
+          lineStyle: {
+            color: color.ZINC_800,
+            type: "dashed",
+            width: 1,
+          },
+          show: true,
+        },
+        axisLabel: {
+          formatter: valueFormatter,
+        },
+      });
+      break;
     }
     default: {
       const dim = opts.dimensions[0];
-      return [
-        {
-          type: "category",
-          show: !["pie", "calendar"].includes(opts.chartType),
-          name: df.columns.get(dim.index) ?? "",
-          nameLocation: "center",
-          nameGap: 30,
-          nameTextStyle: {
-            fontSize: 14,
-          },
+      axes.push({
+        type: "category",
+        show: !["pie", "calendar"].includes(opts.chartType),
+        name: df.columns.get(dim.index) ?? "",
+        nameLocation: "center",
+        nameGap: 30,
+        nameTextStyle: {
+          fontSize: 14,
         },
-      ];
+      });
+      break;
     }
   }
+  return axes;
 }
