c11c7c783c100918c18d39f60e8a9e3aea621f53
Michael Harris <mharris@luabase.com>
2023-06-30 19:03:34 +0000

    feat: add default style factory

---
+++ src/factory.ts
@@ -0,0 +1,37 @@
+import { ChartType, StyleOptions } from "./types";
+
+export function defaultStyleOptions(
+  chartType: ChartType
+): StyleOptions<typeof chartType> {
+  switch (chartType) {
+    case "bar":
+      return {
+        showTitle: true,
+        showToolbox: false,
+        showLegend: true,
+        barStyle: "grouped",
+        orientation: "vertical",
+      };
+    case "line":
+      return {
+        showTitle: true,
+        showToolbox: false,
+        showLegend: true,
+        lineStyle: "point",
+        showArea: false,
+      };
+    case "pie":
+    case "scatter":
+      return {
+        showTitle: true,
+        showToolbox: false,
+      };
+    case "calendar":
+    case "heatmap":
+      return {
+        showTitle: true,
+        showToolbox: false,
+        colorGrouping: "continuous",
+      };
+  }
+}
