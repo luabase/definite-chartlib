7d7dec5be87a85d458878fb6641212ef9d14733e
Michael Harris <mharris@luabase.com>
2023-06-30 21:23:54 +0000

    feat: add value formatter

--- src/formatters.ts
+++ src/formatters.ts
@@ -4,3 +4,9 @@ export const categoryFormatter = (value: string | number): string =>
   String(value).length > 15
     ? String(value).slice(0, 11) + "..." + String(value).slice(-4)
     : String(value);
+
+export const valueFormatter = (value: string | number): string =>
+  Intl.NumberFormat("en-US", {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  }).format(Number(value));
