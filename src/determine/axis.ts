cb15d919d0c057e6c5f888dbb15f2bfdeefbef04
Michael Harris <mharris@luabase.com>
2023-07-03 21:09:18 +0000

    fix: axes for bar and scatter

--- src/determine/axis.ts
+++ src/determine/axis.ts
@@ -3,7 +3,7 @@ import { ChartType, Metric, echarts } from "../types";
 import { DataFrame } from "../dataframe";
 import { categoryFormatter, valueFormatter } from "../formatters";
 import { color } from "../constants";
-import { utils } from "..";
+import * as utils from "../utils";
 
 export function axis<T extends ChartType>(
   chart: Chart<T>,
@@ -11,47 +11,60 @@ export function axis<T extends ChartType>(
   kind: "x" | "y"
 ): echarts.Axis[] {
   let dim = kind === "x";
-  dim =
-    chart.getChartType() !== "scatter" &&
-    chart.getOrientation() !== "horizontal";
+  dim = chart.getChartType() === "scatter" ? false : dim;
+  dim = chart.getStyleOrientation() === "horizontal" ? !dim : dim;
+  dim = chart.getChartType() === "heatmap" ? true : dim;
   const axes: echarts.Axis[] = [];
   if (dim) {
+    const ix = chart.getChartType() === "heatmap" && kind === "y" ? 1 : 0;
+    const name = df.columns.get(chart.getDimensions()[ix].index) ?? "";
     const item: echarts.Axis = {
       show: chart.isCartesian(),
       type: "category",
-      name: df.columns.get(chart.getDimensions()[0].index) ?? "",
-      nameLocation: "center",
-      nameGap: 30,
-      nameTextStyle: { fontSize: 14 },
+      name: utils.string.truncate(name, 42),
+      nameGap: kind === "y" ? 72 : 30,
     };
     if (chart.getChartType() === "bar") {
-      item.nameGap = df.shape.height > 6 ? 55 : 30;
+      item.nameGap = df.shape.height > 6 ? item.nameGap + 25 : item.nameGap;
       item.axisLabel = {
         interval: Math.floor((df.shape.height - 1) / 10),
         rotate: df.shape.height > 6 ? 30 : 0,
         formatter: categoryFormatter,
       };
     }
-    axes.push(item);
+    axes.push(addCommonFeatures(item, kind));
   } else {
-    for (let [k, v] of getMapOfValueAxes(chart).entries()) {
-      if (chart.getChartType() === "scatter") {
-        v = kind === "x" ? [v[0]] : [v[1]];
-      }
-      const name = v.map((v) => df.columns.get(v.index)).join(", ");
-      axes.push({
-        show: chart.isCartesian(),
-        type: "value",
-        name: utils.string.truncate(name, 42),
-        nameLocation: "center",
-        nameGap: kind === "x" ? 30 : 50,
-        nameTextStyle: { fontSize: 14 },
-        splitLine: { lineStyle: { type: "dashed", color: color.ZINC_800 } },
-        axisLabel: { formatter: valueFormatter },
+    const map = getMapOfValueAxes(chart);
+    Array.from(map.keys())
+      .sort() // ensures order is "left", "right"
+      .forEach((k) => {
+        let v = <Metric<T>[]>map.get(k);
+        if (chart.getChartType() === "scatter") {
+          v = kind === "x" ? [v[0]] : [v[1]];
+        }
+        const name = v.map((v) => df.columns.get(v.index)).join(", ");
+        const item: echarts.Axis = {
+          show: chart.isCartesian(),
+          type: "value",
+          name: utils.string.truncate(name, 42),
+          nameGap: kind === "x" ? 30 : 50,
+          axisLabel: { formatter: valueFormatter },
+        };
+        axes.push(addCommonFeatures(item, kind));
       });
-    }
   }
   return axes;
+}
+
+function addCommonFeatures(item: echarts.Axis, kind: "x" | "y") {
+  item.nameLocation = "center";
+  item.nameTextStyle = { fontSize: 14 };
+  if (kind === "y") {
+    item.splitLine = {
+      lineStyle: { type: "dashed", color: color.ZINC_800 },
+    };
+  }
+  return item;
 }
 
 function getMapOfValueAxes<T extends ChartType>(chart: Chart<T>) {
