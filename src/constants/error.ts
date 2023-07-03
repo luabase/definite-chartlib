1aab42ee7218731b4393b4a2087c4a0d9c471409
Michael Harris <mharris@luabase.com>
2023-07-03 21:08:57 +0000

    feat: add invalid chart error

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -1,2 +1,3 @@
 export const DIMENSIONS_NOT_SET = "At least one dimension must be configured";
 export const METRICS_NOT_SET = "At least one metric must be configured";
+export const INVALID_CHART = "Chart is not valid. Check chart options";
