3cbda390a32f7f2378826033e67f996c38e9cbe7
Michael Harris <mharris@luabase.com>
2023-07-04 18:01:03 +0000

    chore: add dev note

--- src/formatters.ts
+++ src/formatters.ts
@@ -1,9 +1,9 @@
-import { Option } from "./types/utility";
-
 export const categoryFormatter = (value: string | number): string =>
   String(value).length > 15
     ? String(value).slice(0, 11) + "..." + String(value).slice(-4)
     : String(value);
+
+// TODO - datetime formatter
 
 export const valueFormatter = (value: string | number): string =>
   Intl.NumberFormat("en-US", {
