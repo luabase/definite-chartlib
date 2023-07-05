6406510a020efebdbf5bd91c5927b065553eff93
Michael Harris <mharris@luabase.com>
2023-07-05 19:51:27 +0000

    chore: document expected ID format

--- src/types/echarts/dataset.ts
+++ src/types/echarts/dataset.ts
@@ -1,6 +1,8 @@
 import { Option } from "../utility";
 
 export default interface DataSet {
+  /** Composite ID ({{ METRIC INDEX }}::{{ CHART TYPE }}::{{ DATASET INDEX }}::{{ NAME }}) */
+  id?: string;
   dimensions: string[];
   source: Array<Array<Option<number | string>>>;
 }
