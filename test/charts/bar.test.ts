bebce454961dfaa96a1b931b470eeaac08806b69
Michael Harris <mharris@luabase.com>
2023-07-05 19:50:43 +0000

    fix: expectations

--- test/charts/bar.test.ts
+++ test/charts/bar.test.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it } from "vitest";
+import * as fixtures from "./fixtures.json";
 
 import chartlib from "../../src";
 import { categoryFormatter, valueFormatter } from "../../src/formatters";
 import { color } from "../../src/constants";
-import * as fixtures from "./fixtures.json";
 
 describe("given 1 dimension and 1 aggregate metric", () => {
   const data = fixtures["dailyUsersByMobileOS"];
@@ -17,7 +17,7 @@ describe("given 1 dimension and 1 aggregate metric", () => {
       aggregation: "sum",
     });
   it("can compile to a vertical bar chart", () => {
-    expect(chartlib.compile("My chart", chart, data)).toEqual({
+    expect(chart.compile("My chart", data)).toEqual({
       animation: true,
       backgroundColor: "#18181b",
       calendar: null,
@@ -28,14 +28,15 @@ describe("given 1 dimension and 1 aggregate metric", () => {
             ["2020-01-01", "iOS", 300],
             ["2020-01-02", "iOS", 310],
             ["2020-01-03", "iOS", 305],
-            ["2020-01-04", "iOS", 305],
+            ["2020-01-03", "iOS", 305],
             ["2020-01-01", "Android", 500],
             ["2020-01-02", "Android", 497],
             ["2020-01-03", "Android", 550],
-            ["2020-01-04", "Android", 580],
+            ["2020-01-03", "Android", 580],
           ],
         },
         {
+          id: "2::bar::1::users",
           dimensions: ["os", "users"],
           source: [
             ["iOS", 1220],
@@ -44,7 +45,7 @@ describe("given 1 dimension and 1 aggregate metric", () => {
         },
       ],
       grid: {
-        bottom: "18%",
+        bottom: "12%",
         containLabel: false,
         left: "12%",
         right: "9%",
@@ -100,10 +101,10 @@ describe("given 1 dimension and 1 aggregate metric", () => {
           axisLabel: {
             formatter: categoryFormatter,
             interval: 0,
-            rotate: 30,
+            rotate: 0,
           },
           name: "os",
-          nameGap: 55,
+          nameGap: 30,
           nameLocation: "center",
           nameTextStyle: {
             fontSize: 14,
@@ -146,11 +147,11 @@ describe("given 2 dimensions and 1 aggregate metric", () => {
     .addMetric({
       index: 2,
       dataType: "value",
-      color: [color.LIME_200, color.DARK_BLUE],
+      color: color.LIME_200,
       aggregation: "sum",
     });
   it("can compile to a vertical bar chart", () => {
-    expect(chartlib.compile("My chart", chart, data)).toEqual({
+    expect(chart.compile("My chart", data)).toEqual({
       animation: true,
       backgroundColor: "#18181b",
       calendar: null,
@@ -161,34 +162,34 @@ describe("given 2 dimensions and 1 aggregate metric", () => {
             ["2020-01-01", "iOS", 300],
             ["2020-01-02", "iOS", 310],
             ["2020-01-03", "iOS", 305],
-            ["2020-01-04", "iOS", 305],
+            ["2020-01-03", "iOS", 305],
             ["2020-01-01", "Android", 500],
             ["2020-01-02", "Android", 497],
             ["2020-01-03", "Android", 550],
-            ["2020-01-04", "Android", 580],
+            ["2020-01-03", "Android", 580],
           ],
         },
         {
+          id: "2::bar::1::iOS",
           dimensions: ["date", "users"],
           source: [
             ["2020-01-01", 300],
             ["2020-01-02", 310],
-            ["2020-01-03", 305],
-            ["2020-01-04", 305],
+            ["2020-01-03", 610],
           ],
         },
         {
+          id: "2::bar::2::Android",
           dimensions: ["date", "users"],
           source: [
             ["2020-01-01", 500],
             ["2020-01-02", 497],
-            ["2020-01-03", 550],
-            ["2020-01-04", 580],
+            ["2020-01-03", 1130],
           ],
         },
       ],
       grid: {
-        bottom: "18%",
+        bottom: "12%",
         containLabel: false,
         left: "12%",
         right: "9%",
@@ -204,7 +205,7 @@ describe("given 2 dimensions and 1 aggregate metric", () => {
           type: "bar",
           color: "#d9f99d",
           datasetIndex: 1,
-          encode: { x: "os", y: "users" },
+          encode: { x: "date", y: "users" },
           name: "iOS",
           yAxisIndex: 0,
         },
@@ -212,7 +213,7 @@ describe("given 2 dimensions and 1 aggregate metric", () => {
           type: "bar",
           color: "#2f4b7c",
           datasetIndex: 2,
-          encode: { x: "os", y: "users" },
+          encode: { x: "date", y: "users" },
           name: "Android",
           yAxisIndex: 0,
         },
@@ -252,10 +253,10 @@ describe("given 2 dimensions and 1 aggregate metric", () => {
           axisLabel: {
             formatter: categoryFormatter,
             interval: 0,
-            rotate: 30,
+            rotate: 0,
           },
-          name: "os",
-          nameGap: 55,
+          name: "date",
+          nameGap: 30,
           nameLocation: "center",
           nameTextStyle: {
             fontSize: 14,
@@ -307,7 +308,7 @@ describe("given 1 dimension and 2 aggregate metrics", () => {
       aggregation: "distinct",
     });
   it("can compile to a vertical bar chart", () => {
-    expect(chartlib.compile("My chart", chart, data)).toEqual({
+    expect(chart.compile("My chart", data)).toEqual({
       animation: true,
       backgroundColor: "#18181b",
       calendar: null,
@@ -323,6 +324,7 @@ describe("given 1 dimension and 2 aggregate metrics", () => {
           ],
         },
         {
+          id: "1::bar::1::event (count)",
           dimensions: ["date", "event"],
           source: [
             ["2020-01-01", 2],
@@ -330,6 +332,7 @@ describe("given 1 dimension and 2 aggregate metrics", () => {
           ],
         },
         {
+          id: "2::bar::2::user_id (distinct)",
           dimensions: ["date", "user_id"],
           source: [
             ["2020-01-01", 1],
@@ -338,7 +341,7 @@ describe("given 1 dimension and 2 aggregate metrics", () => {
         },
       ],
       grid: {
-        bottom: "18%",
+        bottom: "12%",
         containLabel: false,
         left: "12%",
         right: "9%",
@@ -402,10 +405,10 @@ describe("given 1 dimension and 2 aggregate metrics", () => {
           axisLabel: {
             formatter: categoryFormatter,
             interval: 0,
-            rotate: 30,
+            rotate: 0,
           },
-          name: "os",
-          nameGap: 55,
+          name: "date",
+          nameGap: 30,
           nameLocation: "center",
           nameTextStyle: {
             fontSize: 14,
@@ -419,7 +422,7 @@ describe("given 1 dimension and 2 aggregate metrics", () => {
           axisLabel: {
             formatter: valueFormatter,
           },
-          name: "users",
+          name: "",
           nameGap: 50,
           nameLocation: "center",
           nameTextStyle: {
