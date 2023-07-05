3bd47023fbfb0ef3630a33f94d2edab453b30f15
Michael Harris <mharris@luabase.com>
2023-07-05 19:55:23 +0000

    chore: remove error messages

--- src/constants/error.ts
+++
@@ -1,3 +0,0 @@
-export const DIMENSIONS_NOT_SET = "At least one dimension must be configured";
-export const METRICS_NOT_SET = "At least one metric must be configured";
-export const INVALID_CHART = "Chart is not valid. Check chart options";
