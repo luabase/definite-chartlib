f34d08d0dfa338e482a4df58e02de9218cdb97ef
Michael Harris <mharris@luabase.com>
2023-07-09 22:45:14 +0000

    feat: add auto chart factory

---
+++ src/factory.ts
@@ -0,0 +1,128 @@
+import Chart from "./chart";
+import { color } from "./constants";
+import { AggregationType, ChartType, DataType } from "./types";
+import * as utils from "./utils";
+
+type ColumnOptions = { index: number; dataType: string };
+type CreateChartMessage = {
+  type: string;
+  dimensions: ColumnOptions[];
+  metrics: ColumnOptions[];
+};
+
+export class AutoChartFactory {
+  private subsetQ: Array<Array<ColumnOptions>>;
+  private createQ: Array<CreateChartMessage>;
+
+  constructor(opts: Array<ColumnOptions>) {
+    this.subsetQ = utils.array.allCombinations(opts, 2);
+    this.createQ = [];
+    while (this.subsetQ.length > 0) {
+      const subset = this.subsetQ.pop();
+      if (subset) {
+        this.addAllPossibleCreateChartMessagesToQueue(subset);
+      }
+    }
+    this.createQ = utils.array.removeDuplicates(this.createQ).reverse(); // best first
+  }
+
+  private addAllPossibleCreateChartMessagesToQueue(opts: Array<ColumnOptions>) {
+    // NOTE: opts.length >= 2
+    const dims = opts.filter((opt) =>
+      ["category", "datetime"].includes(opt.dataType)
+    );
+    if (dims.length < 1) {
+      return; // not valid combo
+    }
+    const metrics = opts.filter((opt) => opt.dataType === "value");
+    if (dims.length === 1) {
+      if (metrics.length === 1) {
+        if (dims[0].dataType === "category") {
+          ["bar", "pie"].forEach((t) =>
+            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
+          );
+        } else if (dims[0].dataType === "datetime") {
+          ["line", "calendar"].forEach((t) =>
+            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
+          );
+        }
+      } else if (metrics.length === 2) {
+        if (dims[0].dataType === "category") {
+          ["bar", "scatter"].forEach((t) =>
+            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
+          );
+        } else if (dims[0].dataType === "datetime") {
+          ["line", "scatter"].forEach((t) =>
+            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
+          );
+        }
+      } else {
+        if (dims[0].dataType === "category") {
+          this.createQ.push({
+            type: "bar",
+            dimensions: dims,
+            metrics: metrics,
+          });
+        } else if (dims[0].dataType === "datetime") {
+          this.createQ.push({
+            type: "line",
+            dimensions: dims,
+            metrics: metrics,
+          });
+        }
+      }
+    } else if (dims.length === 2) {
+      if (metrics.length === 1) {
+        if (dims[0].dataType === "datetime") {
+          this.createQ.push({
+            type: "line",
+            dimensions: dims,
+            metrics: metrics,
+          });
+        } else {
+          ["bar", "heatmap"].forEach((t) =>
+            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
+          );
+        }
+      } else {
+        return; // not valid combo
+      }
+    } else {
+      return; // not valid combo
+    }
+  }
+
+  getPossibleChart(): Chart<ChartType> {
+    const msg = this.createQ.pop();
+    if (!msg) throw new Error("Create chart queue is empty");
+    const chart = new Chart(<ChartType>msg.type);
+    msg.dimensions.forEach((opt) =>
+      chart.addDimension({
+        index: opt.index,
+        dataType: <Exclude<DataType, "value">>opt.dataType,
+      })
+    );
+    msg.metrics.forEach((opt, i) => {
+      const colorArray = [color.LIME_200, ...color.COLOR_PALETTE];
+      const aggregation: AggregationType = ["scatter", "heatmap"].includes(
+        msg.type
+      )
+        ? "none"
+        : "sum";
+      chart.addMetric({
+        index: opt.index,
+        color: utils.array.unboundedReadItem(colorArray, i),
+        aggregation: aggregation,
+      });
+    });
+    return chart;
+  }
+
+  getAllPossibleCharts(): Array<Chart<ChartType>> {
+    const charts: Array<Chart<ChartType>> = [];
+    while (this.createQ.length > 0) {
+      charts.push(this.getPossibleChart());
+    }
+    return charts;
+  }
+}
---
+++ test/unit/factory.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, it } from "vitest";
+
+import { AutoChartFactory } from "../../src/factory";
+import { color } from "../../src/constants";
+
+describe("given a list of column options", () => {
+  const colOpts = [
+    { index: 0, dataType: "category" },
+    // { index: 1, dataType: "datetime" },
+    // { index: 2, dataType: "category" },
+    // { index: 3, dataType: "category" },
+    { index: 4, dataType: "value" },
+    // { index: 5, dataType: "value" },
+    // { index: 6, dataType: "value" },
+  ];
+  it("can provide a list of charts that can be created", () => {
+    const factory = new AutoChartFactory(colOpts);
+    const charts = factory
+      .getAllPossibleCharts()
+      .map((chart) => chart.getOptions());
+    expect(charts).toEqual([
+      {
+        chartType: "bar",
+        style: {
+          showTitle: true,
+          showToolbox: false,
+          showLegend: true,
+          barStyle: "grouped",
+          orientation: "vertical",
+        },
+        dimensions: [{ index: 0, dataType: "category" }],
+        metrics: [{ index: 4, color: color.LIME_200, aggregation: "sum" }],
+      },
+      {
+        chartType: "pie",
+        style: {
+          showTitle: true,
+          showToolbox: false,
+        },
+        dimensions: [{ index: 0, dataType: "category" }],
+        metrics: [{ index: 4, color: color.LIME_200, aggregation: "sum" }],
+      },
+    ]);
+  });
+});
