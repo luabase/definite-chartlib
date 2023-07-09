31308d7ab6bc2996aa6f30c51dbc950d676af6db
Michael Harris <mharris@luabase.com>
2023-07-09 21:18:09 +0000

    feat: add color as singleton

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -3,3 +3,7 @@ import { color } from "../constants";
 export function asArray(s: string | string[]): string[] {
   return Array.isArray(s) ? s : color.LIME_PALETTE;
 }
+
+export function asSingleton(s: string | string[]): string {
+  return Array.isArray(s) ? s[0] : s;
+}
