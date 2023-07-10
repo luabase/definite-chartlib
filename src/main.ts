6a6a76d74ca890989a05098a09356784c9257ec4
Michael Harris <mharris@luabase.com>
2023-07-10 16:01:17 +0000

    feat: add auto chart generator

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import Chart from "./chart";
+import { AutoChartFactory, ColumnOptions } from "./factory";
 import type { ChartOptions, ChartType } from "./types";
+import * as utils from "./utils";
 
 export function create<T extends ChartType>(type: T): Chart<T> {
   return new Chart(type);
@@ -7,4 +9,14 @@ export function create<T extends ChartType>(type: T): Chart<T> {
 
 export function load<T extends ChartType>(opts: ChartOptions<T>): Chart<T> {
   return Chart.load(opts);
+}
+
+export function* chartGenerator(columns: ColumnOptions[]) {
+  let i = 0;
+  const factory = new AutoChartFactory(columns);
+  const charts = factory.generateAllCharts();
+  while (true) {
+    yield utils.array.unboundedReadItem(charts, i);
+    i++;
+  }
 }
