08b205b7badd16ecb69e1336597896aa7f70c866
Michael Harris <mharris@luabase.com>
2023-07-11 11:33:35 -0400

    feat: allow smart load from legacy opts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import Chart from "./chart";
 import { AutoChartFactory, ColumnOptions } from "./factory";
 import type { ChartOptions, ChartType } from "./types";
+import { LegacyOptions } from "./types/legacy";
 import * as utils from "./utils";
 
 export function create<T extends ChartType>(type: T): Chart<T> {
   return new Chart(type);
 }
 
-export function load<T extends ChartType>(opts: ChartOptions<T>): Chart<T> {
-  return Chart.load(opts);
+export function load<T extends ChartType>(opts: ChartOptions<T> | LegacyOptions<T>): Chart<T> {
+  if ("chartType" in opts) {
+    return Chart.load(opts);
+  } else {
+    return Chart.fromLegacy(opts); 
+  }
 }
 
 export function* chartGenerator(columns: ColumnOptions[]) {
