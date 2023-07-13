0d3f11ebf67e5bc483d25ca3deaf54bc191cb1eb
Michael Harris <mharris@luabase.com>
2023-07-13 20:32:42 +0000

    fix: expose colors

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export * as echarts from "./echarts";
 
 export * from "./aliases";
+export { color } from "../constants";
 export * from "./literals";
 export * from "./option";
 export * from "./utility";
