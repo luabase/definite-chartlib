671071ed22928e8eef0e42e828faa8718716f0ff
Michael Harris <mharris@luabase.com>
2023-07-24 13:51:52 -0400

    fix: reduce cutoff for large set to 6 (#37)

--- src/utils/datasets.ts
+++ src/utils/datasets.ts
@@ -1,5 +1,5 @@
 import { echarts } from "../types";
 
 export function containsLargeData(datasets: echarts.DataSet[]): boolean {
-  return datasets[datasets.length - 1].source.length > 10;
+  return datasets[datasets.length - 1].source.length > 6;
 }
