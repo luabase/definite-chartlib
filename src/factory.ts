11bf96189f30af40ff80d7b7e824b3cd638dad58
Jichao Sun <4977515+JichaoS@users.noreply.github.com>
2023-08-04 02:42:46 -0700

    Refactor and fix ignoring order in chart matching
     - Creates a clear config map of column_type -> chart_types
     - Chart matching now ignores column order

--- src/factory.ts
+++ src/factory.ts
@@ -6,17 +6,67 @@ import * as utils from "./utils";
 export type ColumnOptions = { index: number; dataType: string };
 type CreateChartMessage = {
   type: string;
-  dimensions: ColumnOptions[];
-  metrics: ColumnOptions[];
+  options: Array<ColumnOptions>;
 };
 
 const COLORS = [color.LIME_200, ...color.COLOR_PALETTE.slice(1)];
+
+// Map of column options to chart types, treated as unordered sets.
+// Can match multiple
+export type ChartMatchConfigOption = { column_type: string[]; chart_types: string[] };
+const chartMatchConfig: ChartMatchConfigOption[] = [
+  ...forAddValueColumnType({
+    column_type: ["category"],
+    chart_types: ["bar"],
+  }, 0, COLORS.length),
+  ...forAddValueColumnType({
+    column_type: ["datetime"],
+    chart_types: ["line"],
+  }, 0, COLORS.length),
+  {
+    column_type: ["category", "value"],
+    chart_types: ["pie"],
+  },
+  {
+    column_type: ["datetime", "value"],
+    chart_types: ["calendar"],
+  },
+  {
+    column_type: ["category", "value", "value"],
+    chart_types: ["scatter"],
+  },
+  {
+    column_type: ["datetime", "value", "value"],
+    chart_types: ["scatter"],
+  },
+  {
+    column_type: ["category", "category", "value"],
+    chart_types: ["bar", "heatmap"],
+  },
+  {
+    column_type: ["datetime", "category", "value"],
+    chart_types: ["line", "heatmap", "heatmap"],
+  },
+]
+
+function forAddValueColumnType(column_types: ChartMatchConfigOption, min: number, max: number) {
+    const entries = [];
+    for (let i = min; i <= max; i++) {
+        const newItem = JSON.parse(JSON.stringify(column_types)); // Deep copy the original item
+        for (let j = 0; j < i; j++) {
+            newItem.column_type.push("value");
+        }
+        entries.push(newItem);
+    }
+    return entries;
+}
 
 export class AutoChartFactory {
   private subsetQ: Array<Array<ColumnOptions>>;
   private createQ: Array<CreateChartMessage>;
 
   constructor(opts: Array<ColumnOptions>) {
+    // Unordered.
     this.subsetQ = utils.array.getAllSubsets(opts, 2);
     this.createQ = [];
     while (this.subsetQ.length > 0) {
@@ -29,98 +79,39 @@ export class AutoChartFactory {
   }
 
   private addAllCreateChartMessagesToQueue(opts: Array<ColumnOptions>) {
-    // NOTE: opts.length >= 2
-    const dims = opts.filter((opt) =>
-      ["category", "datetime"].includes(opt.dataType)
+    const column_options = opts.map(opt => opt.dataType);
+
+    // Sort here to ignore order of columns.
+    const matches = chartMatchConfig.filter(config =>
+      config.column_type.length === column_options.length &&
+      config.column_type.sort().join() === column_options.sort().join()
     );
-    if (dims.length < 1) {
-      return; // not supported combo
-    }
-    const metrics = opts.filter((opt) => opt.dataType === "value");
-    if (dims.length === 1) {
-      if (metrics.length === 1) {
-        if (dims[0].dataType === "category") {
-          ["bar", "pie"].forEach((t) =>
-            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
-          );
-        } else if (dims[0].dataType === "datetime") {
-          ["line", "calendar"].forEach((t) =>
-            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
-          );
-        }
-      } else if (metrics.length === 2) {
-        if (dims[0].dataType === "category") {
-          ["bar", "scatter"].forEach((t) =>
-            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
-          );
-        } else if (dims[0].dataType === "datetime") {
-          ["line", "scatter"].forEach((t) =>
-            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
-          );
-        }
-      } else {
-        if (dims[0].dataType === "category") {
-          this.createQ.push({
-            type: "bar",
-            dimensions: dims,
-            metrics: metrics,
-          });
-        } else if (dims[0].dataType === "datetime") {
-          this.createQ.push({
-            type: "line",
-            dimensions: dims,
-            metrics: metrics,
-          });
-        }
-      }
-    } else if (dims.length === 2) {
-      // TODO: if metrics.length === 0, use count and distinct aggregations
-      if (metrics.length === 1) {
-        if (
-          dims[0].dataType === "datetime" &&
-          dims[1].dataType === "category"
-        ) {
-          ["line", "heatmap"].forEach((t) =>
-            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
-          );
-        } else if (
-          dims[0].dataType === "category" &&
-          dims[1].dataType === "category"
-        ) {
-          ["bar", "heatmap"].forEach((t) =>
-            this.createQ.push({ type: t, dimensions: dims, metrics: metrics })
-          );
-        } else if (
-          dims[0].dataType === "category" &&
-          dims[1].dataType === "datetime"
-        ) {
-          this.createQ.push({
-            type: "heatmap",
-            dimensions: dims,
-            metrics: metrics,
-          });
-        } else {
-          return; // not supported combo
-        }
-      } else {
-        return; // not supported combo
-      }
-    } else {
-      return; // not supported combo
-    }
+
+  if (matches.length > 0) {
+    matches.forEach(match => {
+      match.chart_types.forEach(chartType => this.createQ.push({ type: chartType, options: opts }));
+    });
+  } else {
+    return; // Unsupported combination
+  }
   }
 
   private generateSingleChart(): Chart<ChartType> {
     const msg = this.createQ.shift();
     if (!msg) throw new Error("No more charts to generate");
     const chart = new Chart(<ChartType>msg.type);
-    msg.dimensions.forEach((opt) =>
+
+    const valueOptions: ColumnOptions[] = msg.options.filter(opt => opt.dataType === "value");
+    const otherOptions: ColumnOptions[] = msg.options.filter(opt => opt.dataType !== "value");
+
+    otherOptions.forEach((opt) => {
       chart.addDimension({
         index: opt.index,
         dataType: <Exclude<DataType, "value">>opt.dataType,
       })
-    );
-    msg.metrics.forEach((opt, i) => {
+    });
+
+    valueOptions.forEach((opt, i) => {
       const colorChoice = ["pie", "calendar", "heatmap"].includes(msg.type)
         ? color.LIME_PALETTE
         : utils.array.unboundedReadItem(COLORS, i);
@@ -132,7 +123,7 @@ export class AutoChartFactory {
         color: colorChoice,
         aggregation: aggregation,
       });
-    });
+    })
     return chart;
   }
 
