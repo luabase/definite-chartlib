f7f38bd29b29043f88e9a638384acd0f9283c8a0
Michael Harris <mharris@luabase.com>
2023-09-13 15:12:46 +0000

    fix: confine tooltip within window

--- src/determine/tooltip.ts
+++ src/determine/tooltip.ts
@@ -3,11 +3,13 @@ import { ChartType, echarts } from "../types";
 import { calendarTooltipFormatter } from "../formatters";
 
 export function tooltip<T extends ChartType>(chart: Chart<T>): echarts.ToolTip {
+  const isBarOrLine = ["bar", "line"].includes(chart.getChartType());
   const item: echarts.ToolTip = {
+    confine: true,
     show: true,
-    trigger: !["line", "bar"].includes(chart.getChartType()) ? "item" : "axis",
+    trigger: !isBarOrLine ? "item" : "axis",
   };
-  if (["bar", "line"].includes(chart.getChartType())) {
+  if (isBarOrLine) {
     item.axisPointer = { type: "cross", crossStyle: { color: "#999999" } };
   } else if (chart.getChartType() === "calendar") {
     item.formatter = calendarTooltipFormatter;
--- src/types/echarts/tooltip.ts
+++ src/types/echarts/tooltip.ts
@@ -10,6 +10,7 @@ interface AxisPointer {
 }
 
 export default interface ToolTip extends IShowable {
+  confine?: boolean;
   trigger?: "item" | "axis" | "none";
   axisPointer?: AxisPointer;
   formatter?: string | ((params: any) => string);
