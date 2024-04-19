d47af177577b86eb6aebaf6ae3166505d347f022
Stefan Kovac <mali_stefan@msn.com>
2024-04-19 18:16:40 -0400

    fix kpi bug

--- dist/index.cjs
+++ dist/index.cjs
@@ -23789,7 +23789,6 @@ var _Chart = class {
     const chart = new _Chart("kpi");
     chart.setStyleOption("showTitle", this.getStyleShowTitle());
     chart.setStyleOption("showLongNumber", this.getStyleShowLongNumber());
-    chart.addDimension(this.dimensions[0]);
     chart.addMetric({
       index: this.metrics[0].index,
       color: color_exports2.asArray(this.metrics[0].color, theme),
